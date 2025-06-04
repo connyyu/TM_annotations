a0f3d3acafb445a8cdf2fc998663715ee997b2f4
Conny <43175252+connyyu@users.noreply.github.com>
2025-06-04 15:17:19 +0100

    Update TM_annotations.py

--- TM_annotations.py
+++ TM_annotations.py
@@ -37,7 +37,7 @@ st.sidebar.markdown("[DeepTMHMM plot](#tmhmm_plot)")
 st.sidebar.markdown("""
     <a href='https://github.com/connyyu' target='_blank'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png' 
-        style='position: fixed; bottom: 5%; left: 10%; transform: translateX(-50%); width: 30px; height: 30px;'/>
+        style='position: fixed; bottom: 5%; left: 5%; transform: translateX(-50%); width: 30px; height: 30px;'/>
     </a>
 """, unsafe_allow_html=True)
 
@@ -537,10 +537,3 @@ st.markdown(
     The transmembrane topology prediction was generated using [DeepTMHMM](https://dtu.biolib.com/DeepTMHMM).
     """
 )
-
-st.markdown("""
-    <a href='https://github.com/connyyu' target='_blank'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png' 
-        style='position: fixed; bottom: 5%; left: 10%; transform: translateX(-50%); width: 30px; height: 30px;'/>
-    </a>
-""", unsafe_allow_html=True)
